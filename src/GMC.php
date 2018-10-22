6142dfceddfd8e0bc359acb9f2dba795c5dfc752
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-10-21 23:42:49 -0300

    Prueba Scrutinizer 2

--- src/GMC.php
+++ src/GMC.php
@@ -3,6 +3,11 @@
 namespace Generador_Multiple_Choice;
 
 class GMC{
-        $a   =  1 ;
-$b=52;
+    $a   =  1 ;
+    $b=52;
+    function prueba(){
+        if($this->a){
+                return $this->b;
+        }
+    }
 }
