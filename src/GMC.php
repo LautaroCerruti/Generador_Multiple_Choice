03e2f7c03f3d32861a8eb3fdd78171f13f25cdc1
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-10-24 11:39:10 -0300

    Prueba tabbing

--- src/GMC.php
+++ src/GMC.php
@@ -2,11 +2,11 @@
 
 namespace Generador_Multiple_Choice;
 
-class GMC(){
-    public $a   =  1 ;
+class GMC{
+    public $a       =    1  ;
     public $b=52;
     public function prueba(){
-            if ($this->a){
+     if ($this->a){
          return $this->b;
         }
     }
