769cceacf898e634ee0b363aff97cc91f60d6b5e
LautaroCerruti <39503825+LautaroCerruti@users.noreply.github.com>
2018-11-06 17:00:32 -0300

    respuestas incorrectas arreglado

--- src/Pregunta.php
+++ src/Pregunta.php
@@ -16,7 +16,7 @@ class Pregunta
         foreach ($pregunta["respuestas_correctas"] as $respuestacorrecta){
             array_push($this->correctas,$respuestacorrecta);
         }
-        foreach ($pregunta["respuestas_correctas"] as $respuestaincorrecta){
+        foreach ($pregunta["respuestas_incorrectas"] as $respuestaincorrecta){
             array_push($this->incorrectas,$respuestaincorrecta);
         }
         if (isset($pregunta["ocultar_opcion_todas_las_anteriores"])){
