f9b5ceedca8a13c199f7c376069933d1e931fb04
Lautaro Cerruti <lautarocerruti7@gmail.com>
2018-11-22 02:25:49 -0300

    Division de preguntas por cantidad de temas implementada

--- src/Delegador.php
+++ src/Delegador.php
@@ -27,34 +27,42 @@ class Delegador
         shuffle($listaPreg);
         if ($this->cantTemas > $this->cantPreguntas){
             $listaPreg = array_chunk($this->preguntas, $this->cantPreguntas);
+            $this->cantTemas = $this->cantPreguntas;
         }
         else{
         //funcion auxiliar
-        $listaPreg = array_chunk($preguntas, $cantTemas);  
+        $listaPreg = $this->array_divide($preguntas, $cantTemas);  
         }
         return $listaPreg;
-    }
-
-    protected function repartir($listaPreg)
-    {
-   //     ($this->cantPreguntas/$this->cantTemas)
     }
 
     public function crearExamen($listaPreg)
     {
         $i = 0;
         $listExamenes = array();
-        for ($i; $i<$cantTemas; $i++){
+        for ($i; $i<$this->cantTemas; $i++){
             $listaExamenes[$i]=new Examen($listaPreg[$i]);
         }
         return $listaExamenes;
     }
 
 
-
     public function cantPreguntasDisponibles()
     {
         return $this->cantPreguntas;
+    }
+
+    protected function array_divide($array, $segmentCount) {
+        $dataCount = count($array);
+        if ($dataCount == 0) return false;
+        $segmentLimit = floor($dataCount / $segmentCount);
+        $outputArray = array();
+        while($dataCount > $segmentLimit) {
+            $outputArray[] = array_splice($array,0,$segmentLimit);
+            $dataCount = count($array);
+        }
+        if($dataCount > 0) $outputArray[] = $array;
+        return $outputArray;
     }
 
 }
